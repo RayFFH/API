18d91323d8a38605369d6a73e771222fd3882b94
RayFFH <phoenix222smart@gmail.com>
2020-09-27 23:35:25 +0100

    update

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
    id = db.Column(db.Integer, primary_key=True)
    username = db.Column(db.String(100), unique=True, nullable=False)
    email = db.Column(db.String(100),unique=True, nullable=False)
@@ -20,3 +25,5 @@ class Post(db.Model):
    date_posted = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
    content= db.Column(db.Text, nullable=False)
    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+db.create_all()
